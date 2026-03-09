39b5996ccf4e68de35ee4c3416ed1b8c41f4d90d
Gladys Lim <gl3006@hw.ac.uk>
2026-03-09 14:51:59 +0800

    add MedHomepage

---
+++ frontend/src/MedHomepage.jsx
@@ -0,0 +1,95 @@
+import "./MedHomepage.css";
+import UserProfile from "./UserProfile";
+
+export default function Homepage() {
+
+    return (
+        <div className="medhomepage">
+            
+            {/* <Navbar /> */}
+            <div className="content">
+                <h1>Welcome, Dr. Andy</h1>
+                <p>Manage your patient data access and monitor health metrics.</p>
+                <TopRow />
+                <BottomRow />
+            </div>
+        </div>
+    );
+}
+
+function Card({title, description,variant, icon}){
+    return (
+        <div className= {`Card ${variant === "primary" ? "card-primary" : "card-secondary"}`}>
+            <div className="card-icon">
+                {icon}
+            </div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <span className="open"> Open &gt; </span>
+        </div>
+    );
+}
+
+function TopRow() {
+    return (
+        <div className="top-row">
+            <div className="box1">
+                {/* Request Data Access */}
+                <Card 
+                    title="Request Data Access"
+                    description="Submit new patient data access request"
+                    variant="primary"
+                    icon="👤"
+                />
+            </div>
+            <div className="box1">
+                {/* Check Status */}
+                <Card 
+                    title="Check Status"
+                    description="View pending and approved request"
+                    variant="primary"
+                    icon =" "
+                />
+            </div> 
+        </div>
+    );
+}
+
+function BottomRow() {
+    return (
+        <div className="bottom-row">
+            <div className="box2">
+                {/* Total Patients */}
+                <Card 
+                    title="Total Patients"
+                    description="24 persons"
+                    icon=" "
+                />
+            </div>
+            <div className="box2">
+                {/* Pending */}
+                <Card 
+                    title="Pending"
+                    description="5 pending requests"
+                    icon=""
+                />
+            </div>
+            <div className="box2">
+                {/* active access */}
+                <Card 
+                    title="Active access"
+                    description="19 active access"
+                    icon=""
+                />
+            </div>
+            <div className="box2">
+                {/* Chatbot */}
+                <Card 
+                    title="Chatbot"
+                    description="FAQs & Assistance"
+                    icon=""
+                />
+            </div>
+        </div>
+    );
+}
